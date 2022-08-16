cd51b4b5b8bc2c78b1d578eb0eea3a99693fddf9
Crowdbotics <team@crowdbotics.com>
2022-08-16 21:49:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "mansa_market_watch_36819",
+  url: "https://mansa_market_watch_36819.botics.co",
+  api: "https://mansa_market_watch_36819.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
